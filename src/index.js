a88d656a8e8484f80af4236883d57ea63c76434f
Hope Bradley <hope.e.bradley@gmail.com>
2021-03-23 17:37:12 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+let count = 1;
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  function addTask(e) {
+    e.preventDefault();
+    const list = document.getElementById('tasks');
+    const li = document.createElement('li');
+
+    li.innerText = document.getElementById('new-task-description').value;
+    list.appendChild(li);
+    li.dataset.num = count++;
+    const button = document.createElement('button');
+    button.innerText = 'X';
+    li.append(button);
+    button.addEventListener('click', deleteTask);
+    
+    document.getElementById('new-task-description').value = '';
+  }
+
+  document.getElementById('create-task-form').addEventListener('submit', addTask);
+
 });
+
+function deleteTask(e) {
+  e.target.parentElement.remove();
+}
